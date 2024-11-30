3e3270215a518a165ee1da78ced45dc3b66a0378
Arnald Souza <arnaldsouza472@outlook.com.br>
2024-11-29 22:36:29 -0300

    scan p

--- src/main/java/com/project/ac2/controller/HelloWorldController.java
+++ src/main/java/com/project/ac2/controller/HelloWorldController.java
@@ -1,11 +1,11 @@
 package com.project.ac2.controller;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication(scanBasePackages = {"com.project.ac2", "com.project.ac2.controller"})
+
 @RestController
 @RequestMapping("/test")
 public class HelloWorldController {
