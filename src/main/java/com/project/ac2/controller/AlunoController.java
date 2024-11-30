f7170173fac870646d948016584a9f142b54e153
Arnald Souza <arnaldsouza472@outlook.com.br>
2024-11-29 22:18:01 -0300

    ojg adfos-gq´sfo

--- src/main/java/com/project/ac2/controller/AlunoController.java
+++ src/main/java/com/project/ac2/controller/AlunoController.java
@@ -1,13 +1,15 @@
-package com.project.ac2.controller;
+package com.project.ac2.controller;	
 
 import com.project.ac2.service.AlunoService;
 import com.project.ac2.dto.AlunoDTO;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@SpringBootApplication(scanBasePackages = {"com.project.ac2", "com.project.ac2.controller"})
 @RestController
 @CrossOrigin
 @RequestMapping("/api")
